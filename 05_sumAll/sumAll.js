c32c312b5092f05b04b9a25b1467ebcb114c20ab
japhetvance <jvmpamonag@gmail.com>
2024-09-19 04:07:14 +0800

    finish leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstVal, lastVal) {
+    if (typeof firstVal !== 'number' || typeof lastVal !== 'number') {
+        return 'ERROR';
+    }
+    if (firstVal < 0 || lastVal < 0) {
+        return 'ERROR';
+    }
+    if (firstVal > lastVal) {
+        let temp = firstVal;
+        firstVal = lastVal;
+        lastVal = temp;
+    }
+    let sum = 0;
+    for (let i = firstVal; i <= lastVal; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
