e1c36313529ddace5639f8e82271e25f78e81bdc
japhetvance <jvmpamonag@gmail.com>
2024-09-19 02:57:22 +0800

    finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, val) {
+    
+    arr = arr.filter(item => item !== val);
+
+    return arr;
 };
 
 // Do not edit below this line
